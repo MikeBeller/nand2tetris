1a0cd0bc700fcf4d4d364b7e9a93653a49c43443
MikeBeller <bellermike@gmail.com>
2019-09-07 18:12:42 -0400

    Reorg tests into tests module

--- 07/vmtrans/src/main.rs
+++ 07/vmtrans/src/main.rs
@@ -264,104 +264,6 @@ impl Translator {
     }
 }
 
-#[test]
-fn simple_parse_str_test() {
-    assert_eq!(parse_str(""), None);
-    assert_eq!(parse_str("// foo bar"), None);
-    assert_eq!(parse_str("add"), Some(VMCommand::Arithmetic(VMOp::ADD)));
-    assert_eq!(parse_str("pop foo bar"), None);
-    assert_eq!(parse_str("push splat bar"), None);
-    assert_eq!(parse_str("push constant 33"), Some(VMCommand::Push(VMSeg::CONSTANT, 33)));
-    assert_eq!(parse_str("pop local 3"), Some(VMCommand::Pop(VMSeg::LOCAL, 3)));
-}
-
-#[test]
-fn trans_bin_test() {
-    let mut tr = Translator::new("Splat");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::ADD)), 
-        "// add\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M+D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::SUB)), 
-        "// sub\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M-D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::AND)), 
-        "// and\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M&D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::OR)), 
-        "// or\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M|D\n");
-}
-
-#[test]
-fn trans_unary_test() {
-    let mut tr = Translator::new("Splat");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::NEG)), "// neg\n@SP\nA=M-1\nM=-M\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::NOT)), "// not\n@SP\nA=M-1\nM=!M\n");
-}
-
-#[test]
-fn trans_cmp_test() {
-    let mut tr = Translator::new("Splat");
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::EQ)), 
-               "// eq\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
-               "@TST.0\nD;JEQ\n@SP\nA=M\nM=0\n" +
-               "(TST.0)\n@SP\nM=M+1\n"
-               );
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::LT)), 
-               "// lt\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
-               "@TST.1\nD;JLT\n@SP\nA=M\nM=0\n" +
-               "(TST.1)\n@SP\nM=M+1\n"
-               );
-    assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::GT)), 
-               "// gt\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
-               "@TST.2\nD;JGT\n@SP\nA=M\nM=0\n" +
-               "(TST.2)\n@SP\nM=M+1\n"
-               );
-}
-
-#[test]
-fn trans_push_test() {
-    let mut tr = Translator::new("Splat");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::CONSTANT, 33)), 
-        "// push constant 33\n@33\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::LOCAL, 3)), 
-        "// push local 3\n@3\nD=A\n@LCL\nA=M+D\nD=M\n".to_owned() +
-                "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::TEMP, 3)), 
-        "// push temp 3\n@3\nD=A\n@5\nA=A+D\nD=M\n".to_owned() +
-                "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::POINTER, 0)), 
-        "// push pointer 0\n@THIS\nD=M\n".to_owned() +
-                "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::POINTER, 1)), 
-        "// push pointer 1\n@THAT\nD=M\n".to_owned() +
-                "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::STATIC, 9)), 
-        "// push static 9\n@Splat.9\nD=M\n".to_owned() +
-                "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-}
-
-#[test]
-fn trans_pop_test() {
-    let mut tr = Translator::new("Splat");
-    assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::LOCAL, 3)),
-        "// pop local 3\n@LCL\nD=M\n@3\nD=D+A\n@R15\nM=D\n".to_owned() + 
-        "@SP\nAM=M-1\nD=M\n" + 
-        "@R15\nA=M\nM=D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::TEMP, 7)),
-        "// pop temp 7\n@5\nD=A\n@7\nD=D+A\n@R15\nM=D\n".to_owned() + 
-        "@SP\nAM=M-1\nD=M\n" + 
-        "@R15\nA=M\nM=D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::POINTER, 0)),
-        "// pop pointer 0\n@THIS\nD=A\n@R15\nM=D\n".to_owned() + 
-        "@SP\nAM=M-1\nD=M\n" + 
-        "@R15\nA=M\nM=D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::POINTER, 1)),
-        "// pop pointer 1\n@THAT\nD=A\n@R15\nM=D\n".to_owned() + 
-        "@SP\nAM=M-1\nD=M\n" + 
-        "@R15\nA=M\nM=D\n");
-    assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::STATIC, 9)),
-        "// pop static 9\n@Splat.9\nD=A\n@R15\nM=D\n".to_owned() + 
-        "@SP\nAM=M-1\nD=M\n" + 
-        "@R15\nA=M\nM=D\n");
-}
-
 fn main() -> Result<(), std::io::Error> {
     let base = std::env::args().nth(1).expect("usage: $0 <fname.vm>");
     let infile_path = format!("{}.vm", base);
@@ -382,3 +284,106 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_parse_str_test() {
+        assert_eq!(parse_str(""), None);
+        assert_eq!(parse_str("// foo bar"), None);
+        assert_eq!(parse_str("add"), Some(VMCommand::Arithmetic(VMOp::ADD)));
+        assert_eq!(parse_str("pop foo bar"), None);
+        assert_eq!(parse_str("push splat bar"), None);
+        assert_eq!(parse_str("push constant 33"), Some(VMCommand::Push(VMSeg::CONSTANT, 33)));
+        assert_eq!(parse_str("pop local 3"), Some(VMCommand::Pop(VMSeg::LOCAL, 3)));
+    }
+
+    #[test]
+    fn trans_bin_test() {
+        let mut tr = Translator::new("Splat");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::ADD)), 
+            "// add\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M+D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::SUB)), 
+            "// sub\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M-D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::AND)), 
+            "// and\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M&D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::OR)), 
+            "// or\n@SP\nAM=M-1\nD=M\n@SP\nA=M-1\nM=M|D\n");
+    }
+
+    #[test]
+    fn trans_unary_test() {
+        let mut tr = Translator::new("Splat");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::NEG)), "// neg\n@SP\nA=M-1\nM=-M\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::NOT)), "// not\n@SP\nA=M-1\nM=!M\n");
+    }
+
+    #[test]
+    fn trans_cmp_test() {
+        let mut tr = Translator::new("Splat");
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::EQ)), 
+                   "// eq\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
+                   "@TST.0\nD;JEQ\n@SP\nA=M\nM=0\n" +
+                   "(TST.0)\n@SP\nM=M+1\n"
+                   );
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::LT)), 
+                   "// lt\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
+                   "@TST.1\nD;JLT\n@SP\nA=M\nM=0\n" +
+                   "(TST.1)\n@SP\nM=M+1\n"
+                   );
+        assert_eq!(tr.trans_cmd(VMCommand::Arithmetic(VMOp::GT)), 
+                   "// gt\n@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\nM=-1\n".to_owned() +
+                   "@TST.2\nD;JGT\n@SP\nA=M\nM=0\n" +
+                   "(TST.2)\n@SP\nM=M+1\n"
+                   );
+    }
+
+    #[test]
+    fn trans_push_test() {
+        let mut tr = Translator::new("Splat");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::CONSTANT, 33)), 
+            "// push constant 33\n@33\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::LOCAL, 3)), 
+            "// push local 3\n@3\nD=A\n@LCL\nA=M+D\nD=M\n".to_owned() +
+                    "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::TEMP, 3)), 
+            "// push temp 3\n@3\nD=A\n@5\nA=A+D\nD=M\n".to_owned() +
+                    "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::POINTER, 0)), 
+            "// push pointer 0\n@THIS\nD=M\n".to_owned() +
+                    "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::POINTER, 1)), 
+            "// push pointer 1\n@THAT\nD=M\n".to_owned() +
+                    "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Push(VMSeg::STATIC, 9)), 
+            "// push static 9\n@Splat.9\nD=M\n".to_owned() +
+                    "@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+    }
+
+    #[test]
+    fn trans_pop_test() {
+        let mut tr = Translator::new("Splat");
+        assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::LOCAL, 3)),
+            "// pop local 3\n@LCL\nD=M\n@3\nD=D+A\n@R15\nM=D\n".to_owned() + 
+            "@SP\nAM=M-1\nD=M\n" + 
+            "@R15\nA=M\nM=D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::TEMP, 7)),
+            "// pop temp 7\n@5\nD=A\n@7\nD=D+A\n@R15\nM=D\n".to_owned() + 
+            "@SP\nAM=M-1\nD=M\n" + 
+            "@R15\nA=M\nM=D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::POINTER, 0)),
+            "// pop pointer 0\n@THIS\nD=A\n@R15\nM=D\n".to_owned() + 
+            "@SP\nAM=M-1\nD=M\n" + 
+            "@R15\nA=M\nM=D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::POINTER, 1)),
+            "// pop pointer 1\n@THAT\nD=A\n@R15\nM=D\n".to_owned() + 
+            "@SP\nAM=M-1\nD=M\n" + 
+            "@R15\nA=M\nM=D\n");
+        assert_eq!(tr.trans_cmd(VMCommand::Pop(VMSeg::STATIC, 9)),
+            "// pop static 9\n@Splat.9\nD=A\n@R15\nM=D\n".to_owned() + 
+            "@SP\nAM=M-1\nD=M\n" + 
+            "@R15\nA=M\nM=D\n");
+    }
+
+}
