358c3a5b3b3254b72d3556ba07b1e7f8105203f1
MikeBeller <bellermike@gmail.com>
2019-09-10 18:09:16 -0400

    Refactor how parser works.  Need to refactor main still.

--- 08/vmtrans/src/main.rs
+++ 08/vmtrans/src/main.rs
@@ -6,22 +6,24 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 use vmtrans::translator::Translator;
-use vmtrans::parser::parse_str;
+use vmtrans::parser::Parser;
 
 fn main() -> Result<(), std::io::Error> {
     let base = std::env::args().nth(1).expect("usage: $0 <fname.vm>");
     let infile_path = format!("{}.vm", base);
     let outfile_path = format!("{}.asm", base);
-    let infile = File::open(infile_path)?;
+    let infile = File::open(infile_path.to_string())?;
     let rdr = BufReader::new(&infile);
     let mut outfile = File::create(outfile_path)?;
+    let parser = Parser::new(&infile_path);
 
+    // Need to not just unwrap the errors.  Refactor.
     let cmds = rdr.lines()
-        .filter_map(|x| parse_str(&x.unwrap()));
+        .map(|x| parser.parse_str(&x.unwrap()).unwrap());
 
     let mut tr = Translator::new(&base);
     for cmd in cmds {
-        let asm = tr.trans_cmd(cmd);
+        let asm = tr.trans_cmd(cmd.unwrap());
         write!(&mut outfile, "{}", asm).unwrap();
     }
     
--- 08/vmtrans/src/parser.rs
+++ 08/vmtrans/src/parser.rs
@@ -1,47 +1,78 @@
 use crate::types::*;
+use std::fmt;
 
-pub fn parse_str(cmd_str: &str) -> Option<VMCommand> {
-    let ws: Vec<&str> = cmd_str.split_whitespace().collect();
-    if ws.len() < 1 || ws[0] == "//" {
-        return None;
-    }
+pub struct Parser {
+    file_name: String,
+    line_num: i32,
+}
 
-    if let Some(vmc) = VMOp::from_str(ws[0]) {
-        Some(VMCommand::Arithmetic(vmc))
-    } else if ws[0] == "push" {
-        if ws.len() == 3 {
-            if let Some(seg) = VMSeg::from_str(ws[1]) {
-                if let Ok(n) = ws[2].parse::<i32>() {
-                    Some(VMCommand::Push(seg, n))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    } else if ws[0] == "pop" {
-        if ws.len() == 3 {
-            if let Some(seg) = VMSeg::from_str(ws[1]) {
-                if let Ok(n) = ws[2].parse::<i32>() {
-                    Some(VMCommand::Pop(seg, n))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    } else {
-        None
+#[derive(PartialEq)]
+pub struct ParserError {
+    file: String,
+    line: i32,
+    msg: String,
+}
+
+impl fmt::Display for ParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ParserError: File: {}, Line: {} -- {}\n", self.file, self.line, self.msg)
     }
 }
 
+impl fmt::Debug for ParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ParserError: {} {} {}", self.file, self.line, self.msg)
+    }
+}
 
+impl Parser {
+    pub fn new(fname: &str) -> Parser {
+        Parser{file_name: fname.to_string(), line_num: 0}
+    }
+
+    fn parser_error(&self, msg: &str) -> ParserError {
+        ParserError{file: self.file_name.to_owned(), line: self.line_num, msg: msg.to_string()}
+    }
+
+    pub fn parse_str(&self, cmd_str: &str) -> Result<Option<VMCommand>, ParserError> {
+        let ws: Vec<&str> = cmd_str.split_whitespace().collect();
+        if ws.len() < 1 || ws[0] == "//" {
+            Ok(None)
+        } else if let Some(vmc) = VMOp::from_str(ws[0]) {
+            Ok(Some(VMCommand::Arithmetic(vmc)))
+        } else if ws[0] == "push" {
+            if ws.len() == 3 {
+                if let Some(seg) = VMSeg::from_str(ws[1]) {
+                    if let Ok(n) = ws[2].parse::<i32>() {
+                        Ok(Some(VMCommand::Push(seg, n)))
+                    } else {
+                        Err(self.parser_error(&format!("Invalid number in push command: {}", cmd_str)))
+                    }
+                } else {
+                    Err(self.parser_error(&format!("Invalid segment name in push command: {}", cmd_str)))
+                }
+            } else {
+                Err(self.parser_error(&format!("Invalid push command: {}", cmd_str)))
+            }
+        } else if ws[0] == "pop" {
+            if ws.len() == 3 {
+                if let Some(seg) = VMSeg::from_str(ws[1]) {
+                    if let Ok(n) = ws[2].parse::<i32>() {
+                        Ok(Some(VMCommand::Pop(seg, n)))
+                    } else {
+                        Err(self.parser_error(&format!("Invalid number in pop command: {}", cmd_str)))
+                    }
+                } else {
+                    Err(self.parser_error(&format!("Invalid segment name in pop command: {}", cmd_str)))
+                }
+            } else {
+                Err(self.parser_error(&format!("Invalid pop command: {}", cmd_str)))
+            }
+        } else {
+            Err(self.parser_error(&format!("Invalid command: {}", ws[0])))
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -49,12 +80,13 @@ mod tests {
 
     #[test]
     fn simple_parse_str_test() {
-        assert_eq!(parse_str(""), None);
-        assert_eq!(parse_str("// foo bar"), None);
-        assert_eq!(parse_str("add"), Some(VMCommand::Arithmetic(VMOp::ADD)));
-        assert_eq!(parse_str("pop foo bar"), None);
-        assert_eq!(parse_str("push splat bar"), None);
-        assert_eq!(parse_str("push constant 33"), Some(VMCommand::Push(VMSeg::CONSTANT, 33)));
-        assert_eq!(parse_str("pop local 3"), Some(VMCommand::Pop(VMSeg::LOCAL, 3)));
+        let p = Parser::new("foo.vm");
+        assert_eq!(p.parse_str(""), Ok(None));
+        assert_eq!(p.parse_str("// foo bar"), Ok(None));
+        assert_eq!(p.parse_str("add"), Ok(Some(VMCommand::Arithmetic(VMOp::ADD))));
+        assert!(p.parse_str("pop foo bar").is_err());
+        assert!(p.parse_str("push splat bar").is_err());
+        assert_eq!(p.parse_str("push constant 33"), Ok(Some(VMCommand::Push(VMSeg::CONSTANT, 33))));
+        assert_eq!(p.parse_str("pop local 3"), Ok(Some(VMCommand::Pop(VMSeg::LOCAL, 3))));
     }
 }
